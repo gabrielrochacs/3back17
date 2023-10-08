f43c8f6d43efbb114f6af8a9168128320e2898a6
Gsrcordeiro <cordeirogb@hotmail.com>
2023-10-08 15:32:46 -0300

    OK

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ appExpress.use(cors({
 appExpress.use(express.urlencoded({ extended: false }));
 appExpress.use(express.json());
 
-appExpress.use('/clientes', rotaCliente);
-appExpress.use('/vendas', rotaVenda);
-appExpress.use('/produtos', rotaProduto);
+appExpress.use('/cliente', rotaCliente);
+appExpress.use('/venda', rotaVenda);
+appExpress.use('/produto', rotaProduto);
 appExpress.use('/itensVenda', rotaItensVenda); // Adicione a rota de ItensVenda aqui
 
 appExpress.listen(porta, host, () => {
